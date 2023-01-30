1838db4366504eb21894ba2171a7c77360669004
JamesOngICL <73653114+JamesOngICL@users.noreply.github.com>
2023-01-30 12:22:14 +0000

    adding values

--- prac-files/covariance.py
+++ prac-files/covariance.py
@@ -7,10 +7,46 @@ def covariance(locations):
     y = locations[:, 1]
     x_mean = np.mean(x)
     y_mean = np.mean(y)
-    covariance = np.sum((x - x_mean) * (y - y_mean)) / (len(x) - 1)
+    # covariance = np.sum((x - x_mean) * (y - y_mean)) / (len(x) - 1)
+    covariance = np.sum((x - x_mean) * (y - y_mean)) / (len(x)-1)
+
     return np.array([[np.var(x, ddof=1), covariance], [covariance, np.var(y, ddof=1)]])
 
+def covariace_two(arr_param):
+
+    """
+    Get the numpy arrays
+    """
+    arr_param = np.array(arr_param)
+    y_vals = arr_param[:, 1]
+    x_vals = arr_param[:, 0]
+    x_mean = np.mean(x_vals)
+    y_mean = np.mean(y_vals)
+    var_x = np.sum((x_vals-x_mean)**2)/len(x_vals)
+    var_y = np.sum((y_vals-y_mean)**2)/len(x_vals)
+    cov_formula = np.sum((x_vals- x_mean) * (y_vals - y_mean)) / (len(x_vals))
+    return np.array([[var_x, cov_formula], [cov_formula, var_y]])
 
 
-# print("our code: ", covariance([[10,10],[20,10],[15,20],[40,30],[10,10],[10,10],[20,10],[15,20],[40,30],[30,10]]))
+def test_covariance():
+
+    tmp = covariance([[15,10],[20,10],[15,20],[40,30],[10,10],[10,10],[20,10],[15,20],[40,30],[30,13]])
+    print("Omar's code Test: ", tmp)
+    #it seems my code is right because we have a scale factor of 10/9 to map between the found values of covariance. 
+    tmp_2 = covariace_two([[15,10],[20,10],[15,20],[40,30],[10,10],[10,10],[20,10],[15,20],[40,30],[30,13]])
+    print("James' Code Test - scale factor of 9/10",tmp_2)
+
+
+if __name__=='__main__':
+    test_covariance()
+    measured_array = [[0.6,1.5],[1.8,0.1],[1.0,2.0],[1.0,4.1],[1.1,0.7],[1.7,1.9],[1.9,3.5],[1.5,4.7],[0.9,2.8],[1.7,6.9]]
+    print("Omar Output:",covariance(measured_array))
+
+    print("James Output:",covariace_two(measured_array))
+
+
+
+
+
+
 # print("correct code: ", np.cov([[10,10],[20,10],[15,20],[40,30],[10,10],[10,10],[20,10],[15,20],[40,30],[30,10]], rowvar=False))
