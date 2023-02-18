4cb275a90aba1532978f6055dd9904a16b021b54
mc1017 <marco.chan616@gmail.com>
2023-02-18 12:46:54 +0000

    added normalising

---
+++ lab4/normalising.py
@@ -0,0 +1,13 @@
+# Particles_set from ParticlesMCL class
+def sum_of_new_weights(particles_set):
+    total_weight = 0
+    for i in range(len(particles_set)):
+        total_weight += particles_set.weights[i]
+    return total_weight
+
+
+def normalise_weights(particles_set, total_weight):
+    for i in range(len(particles_set)):
+        particles_set.weights[i] = particles_set.weights[i] / total_weight
+    
+    
